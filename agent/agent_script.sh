dce06c958cd39a02780234e7f1de8a69d6fa06ab
Andrew Feldman <abf149@mit.edu>
2016-05-08 05:07:12 -0400

    agent script tries harder during failure

--- agent/agent_script.sh
+++ agent/agent_script.sh
@@ -4,5 +4,13 @@ ip=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
 
 curl -s "http://abf149.scripts.mit.edu/server.py?s="$ip
 
-sleep 1m
+if [ $? -eq 0 ]; then
+    echo OK
+    sleep 1m    
+else
+    echo FAIL
+    sleep 10s    
+fi
+
+
 done
