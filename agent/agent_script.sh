df61ee9cef70d31df3601e6d8be5e472f1b0546e
Andrew Feldman <abf149@mit.edu>
2016-05-08 03:08:54 -0400

    Agent curl call is now silent.

--- agent/agent_script.sh
+++ agent/agent_script.sh
@@ -2,7 +2,7 @@ while :
 do
 ip=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
 
-curl -v "http://abf149.scripts.mit.edu/server.py?s="$ip
+curl -s "http://abf149.scripts.mit.edu/server.py?s="$ip
 
 sleep 1m
 done
